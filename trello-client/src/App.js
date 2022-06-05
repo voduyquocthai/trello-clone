22a6b6980a78fe59d600d77176094163c0114443
Thai Vo <tuan@pattern.co.nz>
2022-06-05 13:08:27 +0700

    Complete based UI design

--- trello-client/src/App.js
+++ trello-client/src/App.js
@@ -5,7 +5,120 @@ function App() {
         <div className='trello-master'>
             <nav className='navbar app'>Navbar App</nav>
             <nav className='navbar board'>Navbar Board</nav>
-            <div className='board-columns'>Board Colum</div>
+            <div className='board-columns'>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.fixthephoto.com/blog/UserFiles/Image/222/fine-art-portrait-photography_640x640.jpg'
+                                alt='trello-images'
+                            />
+                            Tittle: Girl Portrait
+                        </li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.fixthephoto.com/blog/UserFiles/Image/222/fine-art-portrait-photography_640x640.jpg'
+                                alt='trello-images'
+                            />
+                            Tittle: Girl Portrait
+                        </li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.fixthephoto.com/blog/UserFiles/Image/222/fine-art-portrait-photography_640x640.jpg'
+                                alt='trello-images'
+                            />
+                            Tittle: Girl Portrait
+                        </li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.fixthephoto.com/blog/UserFiles/Image/222/fine-art-portrait-photography_640x640.jpg'
+                                alt='trello-images'
+                            />
+                            Tittle: Girl Portrait
+                        </li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.fixthephoto.com/blog/UserFiles/Image/222/fine-art-portrait-photography_640x640.jpg'
+                                alt='trello-images'
+                            />
+                            Tittle: Girl Portrait
+                        </li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.fixthephoto.com/blog/UserFiles/Image/222/fine-art-portrait-photography_640x640.jpg'
+                                alt='trello-images'
+                            />
+                            Tittle: Girl Portrait
+                        </li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.fixthephoto.com/blog/UserFiles/Image/222/fine-art-portrait-photography_640x640.jpg'
+                                alt='trello-images'
+                            />
+                            Tittle: Girl Portrait
+                        </li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                        <li>Believe in your path</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+            </div>
         </div>
     )
 }
